90ca9397f3e629a32097f0261b400c2cb695b15c
Jom <espino.jomarie@gmail.com>
2022-06-09 11:56:08 +0800

    3rd commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-	<header class="text-center bg-green-700 text-white p-2 mb-10">
+	<header class="text-center bg-green-700 text-white p-3 mb-10">
 		<div class="text-2xl md:text-4xl font-bold mb-3">
 			<i class="fa fa-viruses text-lime-200"></i> Covid-19 Tracker
 		</div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
 	<main v-if="loading" class="flex flex-col align-center justify-center text-center">
-		<div class="text-gray-500 text-3xl mt-10 mb-6"> Fetching Data </div>
+		<div class="text-gray-500 text-3xl mt-10 mb-6"> Fetching Data... </div>
 		<img :src="loadingImage" class="w-24 m-auto" alt="loading image">
 	</main>
 
